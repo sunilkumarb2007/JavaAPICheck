3e84c0833c26ee3b60171c0da8ec163e28ddfee5
sunilkumarb2007 <sunilkumarb200703@gmail.com>
2026-08-28 00:53:45 +0530

    Revert "chore: add spring http imports for AI compatibility"
    This reverts commit 271fc332535b7808fa0ce981950e59195f9592d9.

--- payment-service/src/test/java/com/example/payment/service/PaymentServiceTest.java
+++ payment-service/src/test/java/com/example/payment/service/PaymentServiceTest.java
@@ -9,8 +9,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
 
