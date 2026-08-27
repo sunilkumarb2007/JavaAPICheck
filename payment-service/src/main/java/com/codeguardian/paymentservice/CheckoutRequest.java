3a65fb1c8dae5b4184abd844b5a58d67737b5308
Sunil Kumar B <sunilkumarb200703@gmail.com>
2026-08-27 10:07:21 +0530

    Delete payment-service directory

--- payment-service/src/main/java/com/codeguardian/paymentservice/CheckoutRequest.java
+++
@@ -1,4 +0,0 @@
-package com.codeguardian.paymentservice;
-
-public record CheckoutRequest(Long userId, Long orderId, Double amount) {
-}
--- payment-service/src/main/java/com/codeguardian/paymentservice/CheckoutResponse.java
+++
@@ -1,4 +0,0 @@
-package com.codeguardian.paymentservice;
-
-public record CheckoutResponse(String status, String message, String errorCode) {
-}
